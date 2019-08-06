00c9f13feae1be2f9fc4183f9d900d26d06b553c
waruna <shiranwaruna@gmail.com>
2019-08-06 22:42:25 +0530

    login , register , admin home , user home

--- app/src/main/java/com/example/lonin/editProfile.java
+++ app/src/main/java/com/example/lonin/editProfile.java
@@ -6,7 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class Edit_Profile extends AppCompatActivity {
+public class editProfile extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
